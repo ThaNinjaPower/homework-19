a57afc3ee9b11c054b463876ef3b978093382e26
ThaNinjaPower <kevinvannguyen1997@gmail.com>
2020-06-03 22:50:44 -0500

    Search bar now in nav

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -23,7 +23,7 @@ class Navbar extends Component {
     }
 
     handlePageChange = event => {
-        
+
     }
 
     render() {
@@ -35,30 +35,8 @@ class Navbar extends Component {
                 </button>
     
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Sort By
-                        </a>
-                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a className="dropdown-item" href="">Action</a>
-                            <a className="dropdown-item" href="">Another action</a>
-                            <div className="dropdown-divider"></div>
-                            <a className="dropdown-item" href="">Something else here</a>
-                        </div>
-                    </li>
-                </ul>
-                <form className="form-inline my-2 my-lg-0">
-                    <input
-                        className="form-control mr-sm-2"
-                        name="search"
-                        type="search"
-                        placeholder="Search"
-                        aria-label="Search"
-                        value={this.state.search}
-                        onChange={this.handleSearchChange}
-                    />
-                </form>
+                    <ul className="navbar-nav mr-auto">
+                    </ul>
                 </div>
             </nav>
         );
--- client/src/pages/Employees/index.js
+++ client/src/pages/Employees/index.js
@@ -26,15 +26,29 @@ class Employees extends Component {
     render() {
         return (
             <div>
-                <input
-                    className="form-control mr-sm-2"
-                    name="search"
-                    type="search"
-                    placeholder="Search"
-                    aria-label="Search"
-                    value={this.state.search}
-                    onChange={this.handleSearchChange}
-                />
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <a className="navbar-brand" href="#">Employee Tracker</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto">
+                            <form class="form-inline my-2 my-lg-0">
+                                <input
+                                    className="form-control mr-sm-2"
+                                    name="search"
+                                    type="search"
+                                    placeholder="Search"
+                                    aria-label="Search"
+                                    value={this.state.search}
+                                    onChange={this.handleSearchChange}
+                                />
+                            </form>
+                        </ul>
+                    </div>
+                </nav>
+
                 <div className="employee-list">
                     {this.state.alteredList.map(employee =>
                         <EmployeeCard
